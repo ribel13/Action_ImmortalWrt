27a6b3bb3c37572e86eb40b5ecf8e52fdf7d747e
Atho Belo <181945610+ribel13@users.noreply.github.com>
2025-12-26 11:55:29 +0900

    Uncomment feed sources in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git natelol https://github.com/natelol/natelol' >>feeds.conf.default
-#echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed' >>feeds.conf.default
+echo 'src-git natelol https://github.com/natelol/natelol' >>feeds.conf.default
+echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed' >>feeds.conf.default
 echo 'src-git 3ginfo https://github.com/4IceG/luci-app-3ginfo-lite' >>feeds.conf.default
 #echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
